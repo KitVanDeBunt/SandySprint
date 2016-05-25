a6238b8d8979cdbcfd2282e61999ad3c78c3fc16
Kit van de Bunt <kitlvdbunt@hotmail.com>
2016-05-25 02:21:12 +0200

    skybox movable

--- game/render/skyBoxManager.js
+++ game/render/skyBoxManager.js
@@ -6,15 +6,18 @@
 var SkyBoxManager = (function () {
     function SkyBoxManager(scene, ECSengine) {
         // Skybox
-        var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+        this.skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
         var skyboxMaterial = new BABYLON.StandardMaterial("assets/textures/skyBox", scene);
         skyboxMaterial.backFaceCulling = false;
         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/textures/skybox/skybox", scene);
         skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
         skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-        skybox.material = skyboxMaterial;
+        this.skybox.material = skyboxMaterial;
     }
+    SkyBoxManager.prototype.update = function (cameraPosition) {
+        this.skybox.position = cameraPosition;
+    };
     return SkyBoxManager;
 }());
 //# sourceMappingURL=skyBoxManager.js.map
--- game/render/skyBoxManager.ts
+++ game/render/skyBoxManager.ts
@@ -5,15 +5,20 @@
  */
 class SkyBoxManager {
 
+    private skybox: BABYLON.Mesh;
     constructor(scene: BABYLON.Scene, ECSengine: ECS.Engine) {
         // Skybox
-        var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+        this.skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
         var skyboxMaterial = new BABYLON.StandardMaterial("assets/textures/skyBox", scene);
         skyboxMaterial.backFaceCulling = false;
         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/textures/skybox/skybox", scene);
         skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
         skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
         skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-        skybox.material = skyboxMaterial;
+        this.skybox.material = skyboxMaterial;
+    }
+
+    update(cameraPosition: BABYLON.Vector3) {
+        this.skybox.position = cameraPosition;
     }
 }
