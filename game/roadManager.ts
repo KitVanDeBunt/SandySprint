a198e4a7baf9bc9304bba09c3e341c0e89220709
Kit van de Bunt <kitlvdbunt@hotmail.com>
2016-06-08 12:40:36 +0200

    code convention fix

--- game/roadManager.ts
+++ game/roadManager.ts
@@ -5,14 +5,14 @@ class RoadManager {
 
     // TODO : delete spawned objects
 
-    private engine: ECS.Engine;
-    private scene: BABYLON.Scene;
-    private clonesCreated: boolean = false;
-    private roadMeshes: ECS.ComponentAbstractMesh[];
-    private lanes: ComponentStraightLane[][];
-    private roadesSpawned = 0;
-    private abstractMeshComponetType: string;
-    private sceneObjectFactory: SceneObjectSpawnTemplateSetFactory;
+    private _engine: ECS.Engine;
+    private _scene: BABYLON.Scene;
+    private _clonesCreated: boolean = false;
+    private _roadMeshes: ECS.ComponentAbstractMesh[];
+    private _lanes: ComponentStraightLane[][];
+    private _roadesSpawned = 0;
+    private _abstractMeshComponetType: string;
+    private _sceneObjectFactory: SceneObjectSpawnTemplateSetFactory;
     /**
      * list of obstacles on the road
      */
@@ -26,21 +26,21 @@ class RoadManager {
      * @returns returns the lanes currently in game
      */
     get getLanes(): ComponentStraightLane[][] {
-        return this.lanes;
+        return this._lanes;
     }
 
     constructor(engine: ECS.Engine, scene: BABYLON.Scene) {
-        this.lanes = [];
-        this.roadMeshes = [];
+        this._lanes = [];
+        this._roadMeshes = [];
         this.obstacles = [];
         this.sceneObjects = [];
-        this.engine = engine;
-        this.scene = scene;
+        this._engine = engine;
+        this._scene = scene;
 
-        this.abstractMeshComponetType = new ECS.ComponentAbstractMesh(null, null, null).componentType();
+        this._abstractMeshComponetType = new ECS.ComponentAbstractMesh(null, null, null).componentType();
 
         // initialize scene object factory
-        this.sceneObjectFactory = new SceneObjectSpawnTemplateSetFactory(this, engine);
+        this._sceneObjectFactory = new SceneObjectSpawnTemplateSetFactory(this, engine);
 
         this.createRaodPart();
         this.createRaodPart();
@@ -51,43 +51,43 @@ class RoadManager {
      */
     createRaodPart() {
 
-        let roadN: number = this.lanes.length;
+        let roadN: number = this._lanes.length;
 
-        let road: ECS.Entity = this.engine.createEntity();
+        let road: ECS.Entity = this._engine.createEntity();
 
-        let roadPositionComponent: ECS.ComponentTransform = new ECS.ComponentTransform(new BABYLON.Vector3(0, 0, this.roadesSpawned * 14)
+        let roadPositionComponent: ECS.ComponentTransform = new ECS.ComponentTransform(new BABYLON.Vector3(0, 0, this._roadesSpawned * 14)
             , new BABYLON.Vector3(1, 1, 1)
             , BABYLON.Quaternion.Identity()
         );
 
         road.addComponent(roadPositionComponent);
-        this.roadMeshes[roadN] = new ECS.ComponentAbstractMesh(roadPositionComponent, "assets/models/", "road_river.babylon");
+        this._roadMeshes[roadN] = new ECS.ComponentAbstractMesh(roadPositionComponent, "assets/models/", "road_river.babylon");
         
-        road.addComponent(this.roadMeshes[roadN]);
+        road.addComponent(this._roadMeshes[roadN]);
 
-        this.lanes[roadN] = [
-            new ComponentStraightLane(this.roadMeshes[roadN], new BABYLON.Vector3(-0.25, 0, this.roadesSpawned * 14), BABYLON.Vector3.Zero(), this.scene, this.roadesSpawned * 14),
-            new ComponentStraightLane(this.roadMeshes[roadN], new BABYLON.Vector3(0, 0, this.roadesSpawned * 14), BABYLON.Vector3.Zero(), this.scene, this.roadesSpawned * 14),
-            new ComponentStraightLane(this.roadMeshes[roadN], new BABYLON.Vector3(0.25, 0, this.roadesSpawned * 14), BABYLON.Vector3.Zero(), this.scene, this.roadesSpawned * 14)
+        this._lanes[roadN] = [
+            new ComponentStraightLane(this._roadMeshes[roadN], new BABYLON.Vector3(-0.25, 0, this._roadesSpawned * 14), BABYLON.Vector3.Zero(), this._scene, this._roadesSpawned * 14),
+            new ComponentStraightLane(this._roadMeshes[roadN], new BABYLON.Vector3(0, 0, this._roadesSpawned * 14), BABYLON.Vector3.Zero(), this._scene, this._roadesSpawned * 14),
+            new ComponentStraightLane(this._roadMeshes[roadN], new BABYLON.Vector3(0.25, 0, this._roadesSpawned * 14), BABYLON.Vector3.Zero(), this._scene, this._roadesSpawned * 14)
         ];
 
         // set right en left lanes
-        this.lanes[roadN][1].setRightLane = this.lanes[roadN][2];
-        this.lanes[roadN][1].setLeftLane = this.lanes[roadN][0];
-        this.lanes[roadN][0].setRightLane = this.lanes[roadN][1];
-        this.lanes[roadN][2].setLeftLane = this.lanes[roadN][1];
+        this._lanes[roadN][1].setRightLane = this._lanes[roadN][2];
+        this._lanes[roadN][1].setLeftLane = this._lanes[roadN][0];
+        this._lanes[roadN][0].setRightLane = this._lanes[roadN][1];
+        this._lanes[roadN][2].setLeftLane = this._lanes[roadN][1];
 
         // set new lanes as next lanes to previous spawned lanes
         if (roadN > 0.1) {
-            this.lanes[roadN - 1][0].setNextLane = this.lanes[roadN][0];
-            this.lanes[roadN - 1][1].setNextLane = this.lanes[roadN][1];
-            this.lanes[roadN - 1][2].setNextLane = this.lanes[roadN][2];
+            this._lanes[roadN - 1][0].setNextLane = this._lanes[roadN][0];
+            this._lanes[roadN - 1][1].setNextLane = this._lanes[roadN][1];
+            this._lanes[roadN - 1][2].setNextLane = this._lanes[roadN][2];
         }
 
         // add lane components to road entity
-        road.addComponent(this.lanes[roadN][0]);
-        road.addComponent(this.lanes[roadN][1]);
-        road.addComponent(this.lanes[roadN][2]);
+        road.addComponent(this._lanes[roadN][0]);
+        road.addComponent(this._lanes[roadN][1]);
+        road.addComponent(this._lanes[roadN][2]);
 
         this.createLaneObject(
             roadN
@@ -134,9 +134,9 @@ class RoadManager {
             );
         }
 
-        this.sceneObjectFactory.createRandomTemplateSet(roadN,this.scene);
+        this._sceneObjectFactory.createRandomTemplateSet(roadN,this._scene);
 
-        this.roadesSpawned++;
+        this._roadesSpawned++;
     }
 
     /**
@@ -156,9 +156,9 @@ class RoadManager {
         , lane: number
     ) {
         let randomT: number = Math.random();
-        let obstacle: ECS.Entity = this.engine.createEntity();
+        let obstacle: ECS.Entity = this._engine.createEntity();
         let obstacleTransformComponent: ECS.ComponentTransform = new ECS.ComponentTransform(
-            this.lanes[roadN][lane].getPointAtT(randomT)
+            this._lanes[roadN][lane].getPointAtT(randomT)
             , scale
             , rotation
         );
@@ -168,12 +168,12 @@ class RoadManager {
         obstacle.addComponent(obstacleMesh);
 
         // collision
-        obstacleMesh.setCollision(BABYLON.Mesh.CreateCylinder("LaneObject Collider", colliderHeight, colliderWidth, colliderWidth, 0, 0, this.scene));
+        obstacleMesh.setCollision(BABYLON.Mesh.CreateCylinder("LaneObject Collider", colliderHeight, colliderWidth, colliderWidth, 0, 0, this._scene));
         obstacleMesh.setColliderOffset = collisionMeshOffset;
         obstacleMesh.updateCollision();
 
         let arrayPosition: number = this.obstacles.length;
-        this.obstacles[arrayPosition] = new RoadObstacle(obstacleMesh.getCollider, type, obstacle, this.lanes[roadN][lane].getDistanceAtT(randomT));
+        this.obstacles[arrayPosition] = new RoadObstacle(obstacleMesh.getCollider, type, obstacle, this._lanes[roadN][lane].getDistanceAtT(randomT));
     }
 
     /**
@@ -187,7 +187,7 @@ class RoadManager {
      * returns the first lane used in the game
      */
     public get getStartLane(): ComponentLaneBase {
-        return this.lanes[0][1];
+        return this._lanes[0][1];
     }
 
     /**
@@ -196,22 +196,22 @@ class RoadManager {
      */
     update(playerT: number) {
         // spawn road if needed
-        if (playerT + 60 > this.lanes[this.lanes.length - 1][1].getEndT()) {
+        if (playerT + 60 > this._lanes[this._lanes.length - 1][1].getEndT()) {
             this.createRaodPart();
         }
         // delete road if of screen
-        for (let i = 0; i < this.lanes.length; i++) {
-            if (this.lanes[i][0].getStartT < playerT - 20) {
-                this.roadMeshes[i].getParentEntity.destroy();
-                this.roadMeshes.splice(i, 1);
-                this.lanes.splice(i, 1);
+        for (let i = 0; i < this._lanes.length; i++) {
+            if (this._lanes[i][0].getStartT < playerT - 20) {
+                this._roadMeshes[i].getParentEntity.destroy();
+                this._roadMeshes.splice(i, 1);
+                this._lanes.splice(i, 1);
             }
         }
         // delete obstacles if out of view
         for (var i = 0; i < this.obstacles.length; i++) {
             if (this.obstacles[i].spawnDistance < playerT - 10) {
                 // check if object mesh is loaded before destroying it
-                let meshLoaded: boolean = ((<ECS.ComponentAbstractMesh>this.obstacles[i].entity.getComponent(this.abstractMeshComponetType)).meshState == ECS.MeshLoadState.Loaded);
+                let meshLoaded: boolean = ((<ECS.ComponentAbstractMesh>this.obstacles[i].entity.getComponent(this._abstractMeshComponetType)).meshState == ECS.MeshLoadState.Loaded);
                 if (meshLoaded) {
                     this.obstacles[i].entity.destroy();
                     this.obstacles.splice(i, 1);
@@ -222,7 +222,7 @@ class RoadManager {
         for (var i = 0; i < this.sceneObjects.length; i++) {
             if (this.sceneObjects[i].spawnDistance < playerT - 5) {
                 // check if object mesh is loaded before destroying it
-                let meshLoaded: boolean = ((<ECS.ComponentAbstractMesh>this.sceneObjects[i].entity.getComponent(this.abstractMeshComponetType)).meshState == ECS.MeshLoadState.Loaded);
+                let meshLoaded: boolean = ((<ECS.ComponentAbstractMesh>this.sceneObjects[i].entity.getComponent(this._abstractMeshComponetType)).meshState == ECS.MeshLoadState.Loaded);
                 if (meshLoaded) {
                     this.sceneObjects[i].entity.destroy();
                     this.sceneObjects.splice(i, 1);
